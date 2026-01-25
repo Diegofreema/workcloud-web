ac462a2e83ef4d950eac5d0031bb57b69af01751
Diego <diegofreeman78@gmail.com>
2026-01-25 06:37:10 +0100

    Added customer route

---
+++ app/api/customer/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { Polar } from '@polar-sh/sdk';
+
+const polar = new Polar({
+  // server: process.env.SERVER as 'sandbox' | 'production' ?? 'sandbox',
+  server: 'production',
+  accessToken: process.env.POLAR_ACCESS_TOKEN!,
+});
+
+export async function GET(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { id, userId } = body;
+    if (!userId) {
+      return NextResponse.json(
+        {
+          error: 'Missing required fields: Product id or user id',
+        },
+        { status: 400 },
+      );
+    }
+    const customer = await polar.customers.getStateExternal({
+      externalId: userId,
+    });
+    return NextResponse.json({ customer });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
+}
